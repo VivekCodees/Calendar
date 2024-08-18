a0ae883600670fb92998339f46ad5393d98150d6
Vivek <vivsharma571@gmail.com>
2024-08-18 12:29:39 +0530

    Calendar App Pushing All files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CalendarApp from './components/CalendarApp'
+
+function App() {
+  return (
+    <div className="container">
+      <CalendarApp/>
+    </div>
+  )
+}
+
+export default App
